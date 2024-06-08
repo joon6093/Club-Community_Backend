ffd9dfc8fab601783594a4a44383945711c2227b
jeyong <joon6093@naver.com>
2024-06-08 21:07:20 +0900

    test : restdocs 문서화를 위한 MemberControllerTest 작성

--- src/test/java/org/webppo/clubcommunity_backend/builder/TestBuilder.java
+++ src/test/java/org/webppo/clubcommunity_backend/builder/TestBuilder.java
@@ -1,11 +1,14 @@
 package org.webppo.clubcommunity_backend.builder;
 
-import java.time.LocalDateTime;
-
 import org.webppo.clubcommunity_backend.dto.club.ClubDto;
+import org.webppo.clubcommunity_backend.dto.member.MemberDto;
+import org.webppo.clubcommunity_backend.dto.member.MemberSignupRequest;
+import org.webppo.clubcommunity_backend.dto.member.MemberUpdateRequest;
 import org.webppo.clubcommunity_backend.entity.club.Club;
 import org.webppo.clubcommunity_backend.entity.member.Member;
 import org.webppo.clubcommunity_backend.entity.member.type.RoleType;
+
+import java.time.LocalDateTime;
 
 public class TestBuilder {
 
@@ -23,6 +26,68 @@ public class TestBuilder {
 			.email("test")
 			.role(RoleType.ROLE_USER)
 			.build();
+	}
+
+	public static MemberDto createMemberDto() {
+		return new MemberDto(
+				1L,
+				"Test User",
+				"testuser",
+				"testProfileImage",
+				RoleType.ROLE_USER,
+				LocalDateTime.now().minusYears(20), // Make sure it's in the past
+				"M",
+				"Computer Science",
+				"123456",
+				"010-1234-5678",
+				"test@example.com",
+				LocalDateTime.now(),
+				LocalDateTime.now()
+		);
+	}
+
+	public static MemberSignupRequest createMemberSignupRequest() {
+		return new MemberSignupRequest(
+				"Test User",
+				"testpassword",
+				"testuser",
+				"testProfileImage",
+				LocalDateTime.now().minusYears(20), // Make sure it's in the past
+				"M",
+				"Computer Science",
+				"123456",
+				"010-1234-5678",
+				"test@example.com"
+		);
+	}
+
+	public static MemberUpdateRequest createMemberUpdateRequest() {
+		return new MemberUpdateRequest(
+				LocalDateTime.now().minusYears(20), // Make sure it's in the past
+				"M",
+				"Computer Science",
+				"123456",
+				"010-1234-5678",
+				"newemail@example.com"
+		);
+	}
+
+	public static MemberDto createUpdatedMemberDto() {
+		return new MemberDto(
+				1L,
+				"Test User",
+				"testuser",
+				"testProfileImage",
+				RoleType.ROLE_USER,
+				LocalDateTime.now().minusYears(20), // Make sure it's in the past
+				"M",
+				"Computer Science",
+				"123456",
+				"010-1234-5678",
+				"newemail@example.com",
+				LocalDateTime.now(),
+				LocalDateTime.now()
+		);
 	}
 
 	public static Club createClub(Member member) {
