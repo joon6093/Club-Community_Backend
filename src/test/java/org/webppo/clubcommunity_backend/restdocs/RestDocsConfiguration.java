808c0ac1af8a1bd7bb28f5b3a4d61dd8519e26cd
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-06-08 18:19:16 +0900

    setting : rest docs

---
+++ src/test/java/org/webppo/clubcommunity_backend/restdocs/RestDocsConfiguration.java
@@ -0,0 +1,20 @@
+package org.webppo.clubcommunity_backend.restdocs;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.restdocs.operation.preprocess.Preprocessors;
+
+@TestConfiguration
+public class RestDocsConfiguration {
+
+	@Bean
+	public RestDocumentationResultHandler write() {
+		return MockMvcRestDocumentation.document(
+			"{class-name}/{method-name}", // identifier
+			Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
+			Preprocessors.preprocessResponse(Preprocessors.prettyPrint())
+		);
+	}
+}
