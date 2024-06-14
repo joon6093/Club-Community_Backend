2da867515d1dab83b9efa2fdf5ad659bf86b3a77
jeyong <joon6093@naver.com>
2024-06-15 00:07:00 +0900

    fix : 공지사항에서 동아리의 회원임을 검사하지 않는 버그 수정

--- src/main/java/org/webppo/clubcommunity_backend/service/board/notice/NoticeBoardService.java
+++ src/main/java/org/webppo/clubcommunity_backend/service/board/notice/NoticeBoardService.java
@@ -69,6 +69,9 @@ public class NoticeBoardService {
     }
 
     public List<NoticeBoardDto> readAll(Long clubId) {
+        Club club = clubRepository.findById(clubId).orElseThrow(ClubNotFoundException::new);
+        Member member = memberRepository.findById(PrincipalHandler.extractId()).orElseThrow(MemberNotFoundException::new);
+        checkClubMember(member, club);
         List<NoticeBoard> noticeBoards = noticeBoardRepository.findAllByClubId(clubId);
 
         return noticeBoards.stream()
